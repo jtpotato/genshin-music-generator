c29dd6d6644eee210e6fd4afe0b1163482fb56ce
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-09-28 08:44:03 +1000

    create dataset

---
+++ README.md
@@ -0,0 +1,7 @@
+# Changelog
+V1 - Initial model. Included a few violin scores, 3 layers, 128 nodes.
+V2 - removed violin scores. 3 layers, 128 nodes
+V3 - 6 layers, 128 nodes, locally trained.
+
+# Other
+Conda with Python 3.6 is highly recommended.
---
+++ build_dataset.py
@@ -0,0 +1,6 @@
+import os
+import variables
+
+os.system(f"convert_dir_to_note_sequences --input_dir={variables.INPUT_DIR} --output_file={variables.SEQUENCES_TFRECORD} --recursive")
+
+os.system(f"performance_rnn_create_dataset --config={variables.CONFIG} --input={variables.SEQUENCES_TFRECORD} --output_dir={variables.SEQUENCE_EXAMPLES} --eval_ratio=0.10")
