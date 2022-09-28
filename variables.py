894d8e7c5814cffb70c53b75c6619386aa81c044
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-09-28 18:58:20 +1000

    revert choice to commit v1-4 models

--- variables.py
+++ variables.py
@@ -3,7 +3,7 @@ SEQUENCES_TFRECORD="./notesequences.tfrecord"
 SEQUENCE_EXAMPLES="./sequence_examples"
 CONFIG='performance_with_dynamics_compact'
 SEQUENCE_EXAMPLE_TFRECORD="./sequence_examples/training_performances.tfrecord"
-SEQUENCE_EVAL_TFRECORD="./sequence_examples/training_performances.tfrecord"
+SEQUENCE_EVAL_TFRECORD="./sequence_examples/eval_performances.tfrecord"
 HPARAMS="rnn_layer_sizes=[256,256,256]"
 RUN_DIR="./run_dir"
 OUTPUT_DIR="./output"
