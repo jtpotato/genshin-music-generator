6460080befb2ad6850d2617d7d4e0e8f38f14ba7
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-09-28 18:58:49 +1000

    increase nodes per layer

--- variables.py
+++ variables.py
@@ -4,6 +4,6 @@ SEQUENCE_EXAMPLES="./sequence_examples"
 CONFIG='performance_with_dynamics_compact'
 SEQUENCE_EXAMPLE_TFRECORD="./sequence_examples/training_performances.tfrecord"
 SEQUENCE_EVAL_TFRECORD="./sequence_examples/eval_performances.tfrecord"
-HPARAMS="rnn_layer_sizes=[256,256,256]"
+HPARAMS="rnn_layer_sizes=[512,512,512]"
 RUN_DIR="./run_dir"
 OUTPUT_DIR="./output"
