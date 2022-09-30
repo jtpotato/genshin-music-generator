ed5cbd870ee3b0ae4fd11a580523e49677335149
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-09-30 13:06:16 +1000

    move to v6

--- training.py
+++ training.py
@@ -1,5 +1,4 @@
 import os
 import variables
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.system(f"performance_rnn_train --config={variables.CONFIG} --run_dir={variables.RUN_DIR} --hparams={variables.HPARAMS} --sequence_example_file={variables.SEQUENCE_EXAMPLE_TFRECORD} --num_training_steps=10000")
