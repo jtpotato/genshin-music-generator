29e19594055c64ca68d7688309ae1006e8f9ae0c
jtpotato <58995538+jtpotato@users.noreply.github.com>
2022-10-09 14:43:13 +1100

    finish project

--- variables.py
+++ variables.py
@@ -4,7 +4,7 @@ SEQUENCE_EXAMPLES="./sequence_examples"
 CONFIG='performance_with_dynamics_compact'
 SEQUENCE_EXAMPLE_TFRECORD="./sequence_examples/training_poly_tracks.tfrecord"
 SEQUENCE_EVAL_TFRECORD="./sequence_examples/eval_poly_tracks.tfrecord"
-HPARAMS="rnn_layer_sizes=[256,256],learning_rate=0.02"
+HPARAMS="rnn_layer_sizes=[128,128,128],learning_rate=0.01"
 # Use learning_rate 0.001 for finer tuning, learning_rate 0.01 for coarse tuning.
 RUN_DIR="./run_dir"
 OUTPUT_DIR="./output"
